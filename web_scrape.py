96a07fcb6510fcf10683d98575dcd287de9e9423
Caelan Barondess <cbarondess19@gmail.com>
2023-03-19 14:50:29 -0600

    Improve error handling; add url to top of page

--- web_scrape.py
+++ web_scrape.py
@@ -22,20 +22,72 @@ def status(status):
     else:
         print(f"Status: {status}")
 
+def add_hyperlink(paragraph, url, text):
+    """
+    A function that places a hyperlink within a paragraph object.
+
+    :param paragraph: The paragraph we are adding the hyperlink to.
+    :param url: A string containing the required url
+    :param text: The text displayed for the url
+    :return: The hyperlink object
+    """
+
+    # This gets access to the document.xml.rels file and gets a new relation id value
+    part = paragraph.part
+    r_id = part.relate_to(url, docx.opc.constants.RELATIONSHIP_TYPE.HYPERLINK, is_external=True)
+
+    # Create the w:hyperlink tag and add needed values
+    hyperlink = docx.oxml.shared.OxmlElement('w:hyperlink')
+    hyperlink.set(docx.oxml.shared.qn('r:id'), r_id, )
+
+    # Create a w:r element
+    new_run = docx.oxml.shared.OxmlElement('w:r')
+
+
+    # Create a new w:rPr element
+    rPr = docx.oxml.shared.OxmlElement('w:rPr')
+
+    # Change font color
+    color = docx.oxml.shared.OxmlElement('w:color')
+    color.set(docx.oxml.shared.qn('w:val'), '0000EE')
+
+    # Add underline
+    underline = docx.oxml.shared.OxmlElement('w:u')
+    underline.set(docx.oxml.shared.qn('w:val'), 'single')
+
+    # Join all the xml elements together add add the required text to the w:r element
+    rPr.append(color)
+    rPr.append(underline)
+    new_run.append(rPr)
+    new_run.text = text
+    hyperlink.append(new_run)
+
+    paragraph._p.append(hyperlink)
+
+    return hyperlink
+
 def getSite():
     url = input("URL: ")
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
     try:
-        page = requests.get(url, headers=headers)
+        try:
+            page = requests.get(url, headers=headers)
+        except:
+            page = requests.get(url)
+        #status(page.status_code)
+        print(f"Status: {page.status_code}")
     except:
-        page = requests.get(url)
-    status(page.status_code)
+        print("Invalid URL")
+        return 1
 
     soup = BeautifulSoup(page.content, "html.parser")
     print(f"Title:\n\t{soup.title.text}")
-    return soup
+    if page.status_code == 200:
+        return [soup, url]
+    else:
+        return 1
 
-def parseSite(soup):
+def parseSite(soup, url):
     doc = docx.Document()
 
     # Get location path
@@ -61,6 +113,10 @@ def parseSite(soup):
     for items in header:
         for item in items.find_all(["h1"]):
             doc.add_heading(item.text, 0)
+    
+    # Add url to top of the page
+    p = doc.add_paragraph()
+    link = add_hyperlink(p, url, 'Link to pattern')
 
     # Extract text and headings
     for sections in content:
@@ -71,7 +127,7 @@ def parseSite(soup):
                 doc.add_paragraph(item.text)
             elif (item.name == "ul"):
                 for list in item.find_all(["li"]):
-                    doc.add_paragraph(f"- {list.text}")
+                    doc.add_paragraph(f"- {list.text}", style='List Bullet')
 
     doc.save(path)
     print("File saved")
@@ -107,12 +163,12 @@ def menu():
     while not exit:
         #os.system('cls')
         sys.stderr.write("\x1b[2J\x1b[H") # Clear screen but leave scrollback
-        options = " 1. Scrape a website \n 2. Settings \n 3. Exit "
+        options = " Web Scraper\n -------------------\n 1. Scrape a website \n 2. Settings \n 3. Exit "
         choice = int(input(f"{options}\n > "))
 
         if choice == 1:
-            soup = getSite()
-            parseSite(soup)
+            site = getSite()
+            parseSite(site[0], site[1]) if site != 1 else 0
             wait()
         elif choice == 2:
             settings()
